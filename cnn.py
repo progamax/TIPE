1cda2ec82d04438eef9bb2ae67b1eec512b0c18b
progamax <maxenceb1207@gmail.com>
2020-10-15 16:32:27 +0200

    Ajout Visualisation dans matplotlib

--- cnn.py
+++ cnn.py
@@ -35,7 +35,7 @@ normalized_train = train.map(lambda x,y: (normalization_layer(x),y))
 
 model = keras.Sequential()
 #model.add(data_augmentation)
-model.add(keras.layers.Conv2D(16, 3, padding="same", activation="relu"))
+model.add(keras.layers.Conv2D(16, 3, padding="same", activation="relu", input_shape=(256, 256,3)))
 model.add(keras.layers.MaxPooling2D())
 model.add(keras.layers.Conv2D(32, 3, padding="same", activation="relu"))
 model.add(keras.layers.MaxPooling2D())
@@ -62,13 +62,37 @@ def get_run_logdir():
 run_logdir = get_run_logdir()
 tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
 
-checkpoint_cb = keras.callbacks.ModelCheckpoint("cnnFirstTry.h5", save_best_only=True)
+checkpoint_cb = keras.callbacks.ModelCheckpoint("cnn2.h5", save_best_only=True)
 
 
-epochs = 10
+# %%
+epochs = 1
 history = model.fit(
     normalized_train,
     validation_data=val,
     epochs=epochs,
     callbacks=[tensorboard_cb,checkpoint_cb]
 )
+# %%
+# PARTIE VISUALISATION
+modelImageVisualization = keras.Model(inputs=model.inputs, outputs=model.layers[2].output)
+# %%
+feature_maps = modelImageVisualization.predict(val.take(1))
+# %%
+import matplotlib.pyplot as plt
+for i in range(4):
+    ax = plt.subplot(2,2,i+1)
+    plt.imshow(feature_maps[19,:,:,i],cmap="gray")
+plt.show()
+
+#%%
+filters,biases=model.layers[0].get_weights()
+f_min, f_max = filters.min(), filters.max()
+filters = (filters - f_min) / (f_max - f_min)
+for i in range(6):
+    for j in range(3):
+        ax = plt.subplot(6, 3, (j+1) + i * 3)
+        plt.imshow(filters[:,:,j,i], cmap="gray")
+
+plt.show()
+# %%
